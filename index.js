4c155d2b61721afeef6ebaeb2ec92af2af1310b2
paige <paigetetz@gmail.com>
2022-09-12 02:02:51 -0400

    commit tree 6a97c6d10f2486593d2cf2696b25f8dd62d295a2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution in this file!
+
+
+const employees = {
+    name: value1,
+    streetAddress: value2,
+}
+function updateEmployeeWithKeyandValue(obj, key, value){
+    const newEmployees ={...employees};
+}
